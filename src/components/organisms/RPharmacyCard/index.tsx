088f764a9ecae45329c08d669efad77d64effdce
Serhan Coşkun <grafikri@gmail.com>
2019-08-27 21:19:10 +0300

    theme feature added to test

--- src/components/organisms/RPharmacyCard/index.tsx
+++ src/components/organisms/RPharmacyCard/index.tsx
@@ -1,10 +1,18 @@
 import React from "react"
+import {
+  Card,
+  CardContent,
+  Typography,
+  CardActions,
+  Button
+} from "@material-ui/core"
 import { Theme, WithStyles, withStyles, createStyles } from "@material-ui/core"
 
 const styles = (theme: Theme) =>
   createStyles({
     root: {
-      color: "red"
+      color: "red",
+      margin: theme.spacing()
     }
   })
 
@@ -14,7 +22,29 @@ class RPharmacyCard extends React.Component<StyleProps> {
   componentDidMount() {}
 
   render() {
-    return <div className={this.props.classes.root}>Card</div>
+    return (
+      <div className={this.props.classes.root}>
+        <Card>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Word of the Day
+            </Typography>
+            <Typography variant="h5" component="h2">
+              be lent
+            </Typography>
+            <Typography color="textSecondary">adjective</Typography>
+            <Typography variant="body2" component="p">
+              well meaning and kindly.
+              <br />
+              {'"a benevolent smile"'}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+      </div>
+    )
   }
 }
 
