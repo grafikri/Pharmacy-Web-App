969e94eda576fbcf6961e63553034e119cb924b8
Serhan Coşkun <grafikri@gmail.com>
2019-08-27 21:19:38 +0300

    top level theme injected to component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,23 @@ import React from "react"
 import logo from "./logo.svg"
 //import "./App.css"
 import Home from "./components/Home"
+import { Theme, createMuiTheme } from "@material-ui/core"
 import RPharmacyCard from "./components/organisms/RPharmacyCard"
+
+import { ThemeProvider } from "@material-ui/styles"
+
+const theme = createMuiTheme({
+  palette: {
+    type: "dark"
+  }
+})
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      {/* <Home /> */}
-      <RPharmacyCard />
+      <ThemeProvider theme={theme}>
+        <RPharmacyCard />
+      </ThemeProvider>
     </div>
   )
 }
