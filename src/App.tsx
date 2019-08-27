96d5a2adbb68a100f2dbf75acdd602d546672b47
Serhan Coşkun <grafikri@gmail.com>
2019-08-27 19:10:13 +0300

    PharmacyCard added to App component to show it.

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react"
 import logo from "./logo.svg"
 //import "./App.css"
 import Home from "./components/Home"
+import RPharmacyCard from "./components/organisms/RPharmacyCard"
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <Home />
+      {/* <Home /> */}
+      <RPharmacyCard />
     </div>
   )
 }
