4841a1e1072c33c38ac08d58c7544d76ee3c6f62
Serhan Coşkun <grafikri@gmail.com>
2019-08-27 19:09:54 +0300

    material ui styles connected to component via HOC method.

---
+++ src/components/organisms/RPharmacyCard/index.tsx
@@ -0,0 +1,22 @@
+import React from "react"
+import { Theme, WithStyles, withStyles, createStyles } from "@material-ui/core"
+
+const styles = (theme: Theme) =>
+  createStyles({
+    root: {
+      color: "red"
+    }
+  })
+
+export interface StyleProps extends WithStyles<typeof styles> {}
+
+class RPharmacyCard extends React.Component<StyleProps> {
+  componentDidMount() {}
+
+  render() {
+    console.log("c: ")
+    return <div className={this.props.classes.root}>Card</div>
+  }
+}
+
+export default withStyles(styles)(RPharmacyCard)
