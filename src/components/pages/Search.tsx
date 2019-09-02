38153b927a95c7bca7055139c7a83cefd9b10f04
Serhan Coşkun <grafikri@gmail.com>
2019-09-02 09:17:21 +0300

    distance calculates method seperated to helpers.ts

--- src/components/pages/Search.tsx
+++ src/components/pages/Search.tsx
@@ -1,69 +1,14 @@
 import React from "react"
-import { distance } from "../../helpers"
+import { distance, calculatesLocations } from "../../helpers"
 import { Pharmacy, Coordinate } from "../../appInterfaces"
 
 import { default as SearchTemplate } from "../templates/Search"
-import datas from "../../pharmacies.json"
+
 import _ from "lodash"
 
 class Search extends React.Component {
   state = {
     list: []
-  }
-
-  constructor(props: {}) {
-    super(props)
-    this.calculatesLocations = this.calculatesLocations.bind(this)
-  }
-
-  /**
-   * It brings new locations depend on coordinate
-   *
-   * @param lat latitue
-   * @param lng longitude
-   */
-  calculatesLocations(lat: number, lng: number) {
-    const pharmacies: Pharmacy[] = datas.data.map(item => {
-      const location: Coordinate =
-        item.konum == null
-          ? {}
-          : { lat: +item.konum.split(",")[0], long: +item.konum!.split(",")[1] }
-      return {
-        name: item.eczane_adi,
-        location,
-        address: item.eczane_ilce + " - " + item.eczane_adres,
-        phone: item.phone
-      }
-    })
-
-    let radius = 15
-    let locations = pharmacies.map(item => ({
-      ...item,
-      distance: distance(
-        lat,
-        lng,
-        item.location == null ? 0 : item.location.lat!,
-        item.location == null ? 0 : item.location.long!,
-        "K"
-      )
-    }))
-
-    const myLocations = locations.filter(item => item.distance < radius)
-    const orderedMyLocations = _.orderBy(myLocations, ["distance"], ["asc"])
-
-    const myLocationsWithDistance: Pharmacy[] = orderedMyLocations.map(
-      item => ({
-        ...item,
-        distance: item.distance
-          .toString()
-          .substr(0, 4)
-          .concat(" km")
-      })
-    )
-
-    this.setState({
-      list: myLocationsWithDistance
-    })
   }
 
   navigateToGoogleMaps(lat: number, lng: number) {
@@ -90,7 +35,9 @@ class Search extends React.Component {
     return (
       <SearchTemplate
         submitCoordinate={(lat, lng) => {
-          this.calculatesLocations(lat, lng)
+          this.setState({
+            list: calculatesLocations(lat, lng)
+          })
         }}
         handleClickGoogleMap={(lat, lng) => {
           this.navigateToGoogleMaps(lat, lng)
